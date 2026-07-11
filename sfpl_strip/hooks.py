07c2853c359ff6818d96bec394cbe3df7af5f5b4
gangani9998 <gangani.9998@gmail.com>
2026-07-11 15:21:47 +0530

    chore: remove workflow fixtures

--- sfpl_strip/hooks.py
+++ sfpl_strip/hooks.py
@@ -10,11 +10,11 @@ app_license = "mit"
 
 # required_apps = []
 
-fixtures = [
-    "Workflow",
-    "Workflow State",
-    "Workflow Action Master"
-]
+# fixtures = [
+#     "Workflow",
+#     "Workflow State",
+#     "Workflow Action Master"
+# ]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
