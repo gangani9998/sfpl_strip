b8ecfa4dc5389e48f76520bdea1ded20239e91be
gangani9998 <gangani.9998@gmail.com>
2026-07-11 15:53:41 +0530

    fix: export missing Die and Sizer doctype schemas

---
+++ sfpl_strip/sfpl_strip/doctype/sizer/sizer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Jay Kumar Gangani and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Sizer(Document):
+	pass
